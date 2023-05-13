e2d888c520bd8d2fc2a116130f3e58ce2cbfd20f
nyuudo <hello@nyuudo.com>
2023-05-13 12:50:55 +0200

    Exercise 3

---
+++ src/components/Escena.styles.tsx
@@ -0,0 +1,10 @@
+// import styled components
+import styled from "styled-components";
+
+export const StyledDiv = styled.div`
+  text-align: center;
+  border: 2px solid;
+  border-radius: 32px;
+  padding: 8px 16px;
+  margin: 16px;
+`;
--- src/components/Escena.tsx
+++ src/components/Escena.tsx
@@ -1,4 +1,6 @@
+// import stlye form styles file
+import { StyledDiv } from "./Escena.styles";
 // added property type to PROPS
 export const Escena = ({ text }: { text: string }) => {
-  return <div>{text}</div>;
+  return <StyledDiv>{text}</StyledDiv>;
 };
